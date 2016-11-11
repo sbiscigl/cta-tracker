59467d6f50da6386eb2d3e29fb70ad203019bcd2
Sam Bisciglia <sam.bisciglia@uptake.com>
2016-11-11 16:45:02 -0600

    wip - started to write a closest train algo

--- track.js
+++ track.js
@@ -2,16 +2,29 @@ var responsebutton = document.getElementById('get-response-button');
 var responseLabel  = document.getElementById('response');
 
 /*Get your own key or ask very nicely*/
-var ctaToken = "haha"
+var ctaToken = "815ee6119dbf4adead0b9fb54cf24d5e"
 
 responsebutton.onclick = responseListener
+
+function returnClosest(list, direction) {
+  closestTrain = list[0]
+  list.forEach(function(train) {
+    if (train.prdt < closestTrain.prdt && train.stpDe === direction) {
+      closestTrain = train
+    }
+  })
+  return closestTrain
+}
 
 function responseListener() {
   var req = new XMLHttpRequest();
   req.onreadystatechange = function () {
     if (req.readyState === 4) {
       console.log(req.responseText);
-      responseLabel.textContent = req.responseText;
+      var etaList =  JSON.parse(req.responseText).ctatt.eta;
+      var oHareTrain = closestTrain(etaList, "Service toward O'Hare")
+      var loopTrain = closestTrain(etaList, "Service toward Forest Park")
+      responseLabel.textContent = etaList;
     }
   };
   var testRequest = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + ctaToken + "&mapid=40670" + "&outputType=JSON"
