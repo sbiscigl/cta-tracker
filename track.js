72f32725498e94672bbcafdc5d8c5f2cd531338d
Samuel Bisciglia <samuelbisciglia@Samuels-MacBook-Pro.local>
2016-11-14 18:40:15 -0600

    getting around coors

--- track.js
+++ track.js
@@ -22,13 +22,13 @@ function responseListener() {
     if (req.readyState === 4) {
       console.log(req.responseText);
       var etaList =  JSON.parse(req.responseText).ctatt.eta;
-      var oHareTrain = closestTrain(etaList, "Service toward O'Hare")
-      var loopTrain = closestTrain(etaList, "Service toward Forest Park")
+      //var oHareTrain = closestTrain(etaList, "Service toward O'Hare")
+      //var loopTrain = closestTrain(etaList, "Service toward Forest Park")
       responseLabel.textContent = etaList;
     }
   };
   var testRequest = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + ctaToken + "&mapid=40670" + "&outputType=JSON"
-  req.withCredentials = true;
+  console.log("the test request: " + testRequest);
   req.open("GET", testRequest);
   req.send()
 }
