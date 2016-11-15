c7552d7eb37f117f83762b721f13055ff7d7b253
Samuel Bisciglia <samuelbisciglia@Samuels-MacBook-Pro.local>
2016-11-14 20:48:29 -0600

    added all of the logic

--- track.js
+++ track.js
@@ -1,34 +1,48 @@
-var responsebutton = document.getElementById('get-response-button');
-var responseLabel  = document.getElementById('response');
+var ohareResponseLabel  = document.getElementById('ohare-response');
+var loopResponseLabel  = document.getElementById('loop-response');
 
 /*Get your own key or ask very nicely*/
-var ctaToken = "815ee6119dbf4adead0b9fb54cf24d5e"
+var ctaToken = "dontspeakfortwoyears"
 
-responsebutton.onclick = responseListener
-
-function returnClosest(list, direction) {
-  closestTrain = list[0]
-  list.forEach(function(train) {
-    if (train.prdt < closestTrain.prdt && train.stpDe === direction) {
-      closestTrain = train
-    }
-  })
-  return closestTrain
+function getTimeTillArrivalInMinutes(dateString) {
+  now = new Date();
+  arr = new Date(dateString);
+  dateTimeOffsetInMilliseconds = now.getTimezoneOffset() * 60000
+  diffInMinutes = (arr - (now.getTime() - dateTimeOffsetInMilliseconds))/60000;
+  if (Math.floor(diffInMinutes) <= 0) {
+    return "due"
+  } else {
+    return Math.floor(diffInMinutes)
+  }
 }
 
-function responseListener() {
+function returnClosest(list, direction) {
+  closestTrain = null;
+  list.forEach(function(train) {
+    if (closestTrain === null && train.stpDe === direction) {
+      closestTrain = train
+    } else if (closestTrain !== null && train.arrT < closestTrain.arrT && train.stpDe === direction) {
+      closestTrain = train;
+    }
+  })
+  return closestTrain;
+}
+
+function getTimes() {
+  console.log("interval");
   var req = new XMLHttpRequest();
   req.onreadystatechange = function () {
     if (req.readyState === 4) {
-      console.log(req.responseText);
       var etaList =  JSON.parse(req.responseText).ctatt.eta;
-      //var oHareTrain = closestTrain(etaList, "Service toward O'Hare")
-      //var loopTrain = closestTrain(etaList, "Service toward Forest Park")
-      responseLabel.textContent = etaList;
+      var oHareTrain = returnClosest(etaList, "Service toward O'Hare");
+      var loopTrain = returnClosest(etaList, "Service toward Forest Park")
+      ohareResponseLabel.textContent = "train for ohare arriving at: " + getTimeTillArrivalInMinutes(oHareTrain.arrT) + " minutes";
+      loopResponseLabel.textContent = "train for the loop arriving at: " + getTimeTillArrivalInMinutes(loopTrain.arrT) + " minutes";
     }
   };
   var testRequest = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + ctaToken + "&mapid=40670" + "&outputType=JSON"
-  console.log("the test request: " + testRequest);
   req.open("GET", testRequest);
   req.send()
 }
+
+window.setInterval(getTimes,1000)
